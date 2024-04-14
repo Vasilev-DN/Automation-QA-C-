f33082e45e5c29d2bcfb8f208f85f3370eb82780
Vasilev-DN <159131505+Vasilev-DN@users.noreply.github.com>
2024-04-14 06:08:35 +0300

    Add files via upload

---
+++ Simple-Commands/P02-Square-Area/Program.cs
@@ -0,0 +1,5 @@
+﻿int side = int.Parse(Console.ReadLine());
+
+int area = side * side;
+
+Console.WriteLine(area);
---
+++ Simple-Commands/P04-Trapezoid-Area/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = int.Parse(Console.ReadLine());  
+int b = int.Parse(Console.ReadLine());
+int h = int.Parse(Console.ReadLine());
+
+int area = (a + b) / 2 * h;
+
+Console.WriteLine(area);
