c38c638775e1d594cf52e5b0f939893d389d6b67
Vasilev-DN <159131505+Vasilev-DN@users.noreply.github.com>
2024-04-22 15:43:15 +0300

    Add files via upload

---
+++ While-Loop/04-Sequence-2k + 1/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = int.Parse(Console.ReadLine());
+int counter = 1;
+
+while (counter <= num)
+{
+    Console.WriteLine(counter);
+    counter = counter * 2 + 1;
+}
---
+++ While-Loop/06-Number-in-Range/Program.cs
@@ -0,0 +1,26 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (number >= 1 && number <= 100)
+    {
+        break;
+    }
+
+    number = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(number);
+
+
+//int number = int.Parse(Console.ReadLine());
+
+//bool isInRange = number >= 1 && number <= 100;
+
+//while (!isInRange)
+//{
+    //number = int.Parse(Console.ReadLine());
+    //isInRange = number >= 1 && number <= 100;
+//}
+
+//Console.WriteLine(number);
